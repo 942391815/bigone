9a84564ca3863a372e24c783598b667fdead038d
qiaogu <qiaogu@xiaomi.com>
2022-07-03 21:51:55 +0800

    二叉树递归遍历

---
+++ test-video/src/main/java/beans/TreeNode.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class TreeNode {
+    public int i;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int i) {
+        this.i = i;
+    }
+
+    public TreeNode() {
+    }
+}
---
+++ test-video/src/main/java/utils/BinaryTree.java
@@ -0,0 +1,22 @@
+package utils;
+
+import beans.TreeNode;
+
+public class BinaryTree {
+    public static TreeNode buildTree() {
+        TreeNode one = new TreeNode(1);
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+        TreeNode six = new TreeNode(6);
+        TreeNode seven = new TreeNode(7);
+        one.left = two;
+        one.right = three;
+        two.left = four;
+        two.right = five;
+        three.left = six;
+        three.right = seven;
+        return one;
+    }
+}
